1e2e31d6f7e9184de4c122cdb94367280c891df0
Msukoo <rnalsttnn@gmail.com>
2022-05-09 21:34:38 +0900

    first commit

---
+++ src/main/java/hello/advanced/app/v0/OrderRepositoryV0.java
@@ -0,0 +1,28 @@
+package hello.advanced.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import static java.lang.Thread.sleep;
+
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+    public void save(String itemId) {
+        // 저장 로직
+        if(itemId.equals("ex")){
+            throw new IllegalStateException("예외발생!");
+        }
+        sleep(1000); // 상품을 저장하는데 1초정도 걸린다고 가정.
+    }
+
+    private void sleep(int millis){
+        try{
+            Thread.sleep(millis);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
