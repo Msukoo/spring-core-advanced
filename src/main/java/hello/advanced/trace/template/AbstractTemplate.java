43415f7c8c862173a78d0e3e2c063e5d9133cdd2
Msukoo <rnalsttnn@gmail.com>
2022-05-22 16:57:06 +0900

    test: 전략패턴 예제

--- src/main/java/hello/advanced/trace/template/AbstractTemplate.java
+++ src/main/java/hello/advanced/trace/template/AbstractTemplate.java
@@ -6,6 +6,15 @@ import hello.advanced.trace.logtrace.LogTrace;
 public abstract class AbstractTemplate<T> { // 반환타입 정의
     private final LogTrace trace;
 
+    // 작업에서 알고리즘의 골격을 정의하고 일부 단계를 하위 클래스로 연기
+    // -> 하위 클래스가 알고리즘의 구조를 변경하지 않고도 알고리즘의 특정 단게를 재정의할 수 있음.
+
+    // But
+    // 상속을 사용하기 때문에 상속의 단점을 모두 안고감.
+    // 자식 클래스 입장에서는 부모클래스의 기능을 전혀 사용하지 않는데 그럼에도 불구하고 탬플릿 메서드 패턴을 위해 상속하고 있음 (컴파일 시점에 강하게 결합되는 문제)
+    // 이러한 강한 결합은 좋은 설계가 아님!!
+    // 부모클래스를 수정하면 자식 클래스에도 영향을 줄 수 있다.
+
     public AbstractTemplate(LogTrace trace){
         this.trace = trace;
     }
---
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -0,0 +1,102 @@
+package hello.advanced.trace.strategy;
+
+import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV1Test {
+
+    @Test
+    void strategyTest(){
+        logic1();
+        logic2();
+    }
+
+    private void logic1(){
+        long startTime = System.currentTimeMillis();
+        // 비즈니스 로직 실행
+        log.info("비즈니스 로직1 실행");
+        // 비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    private void logic2(){
+        long startTime = System.currentTimeMillis();
+        // 비즈니스 로직 실행
+        log.info("비즈니스 로직2 실행");
+        // 비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    /**
+     * 전략 패턴 사용
+     * -> 변하지 않는 부분을 Context, 변하는 부분을 Strategy 라는 인터페이스를 만들고 해당 인터페이스를 구현하도록 해서 문제를 해결
+     */
+    @Test
+    void strategyV1(){
+        StrategyLogic1 strategyLogic1 = new StrategyLogic1();
+        ContextV1 contextV1 = new ContextV1(strategyLogic1);
+        contextV1.execute();
+
+        StrategyLogic2 strategyLogic2 = new StrategyLogic2();
+        ContextV1 contextV2 = new ContextV1(strategyLogic2);
+        contextV2.execute();
+    }
+
+    @Test
+    void strategyV2(){
+        Strategy strategyLogic1 = new Strategy(){
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        ContextV1 context1 = new ContextV1(strategyLogic1);
+        context1.execute();
+
+        Strategy strategyLogic2 = new Strategy(){
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+        ContextV1 context2 = new ContextV1(strategyLogic1);
+        context2.execute();
+    }
+
+    @Test
+    void strategyV3(){
+        ContextV1 context1 = new ContextV1(new Strategy(){
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+        context1.execute();
+
+        ContextV1 context2 = new ContextV1(new Strategy(){
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        });
+        context2.execute();
+    }
+
+    @Test
+    void strategyV4(){
+        ContextV1 context1 = new ContextV1(() -> log.info("비즈니스 로직1 실행"));
+        context1.execute();
+
+        ContextV1 context2 = new ContextV1(() -> log.info("비즈니스 로직2 실행"));
+        context2.execute();
+    }
+}
